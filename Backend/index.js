3d6898c2484d0eb2ec0e173b029a4a9d2dbdd644
Dev-KimDonghyun <hyungus723@gmail.com>
2025-04-03 08:38:18 +0900

    Study: Backend(Express) Connect To MongoDB

--- Backend/index.js
+++ Backend/index.js
@@ -1,3 +1,5 @@
+const { mongoURI } = require('./security.js');
+const mongoose = require('mongoose'); // Import Mongoose
 const express = require('express'); // Express Default Setting
 const app = express(); // Express Default Setting
 const PORT = 3000; // Express Default Setting
@@ -5,6 +7,12 @@ const PORT = 3000; // Express Default Setting
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+
+mongoose.connect(mongoURI).then(() => {
+    console.log('Connected MongoDB');
+}).catch((error) => {
+    console.log('Fail to Connect MongoDB', error);
+}); // Connect MongoDB
 
 app.listen(PORT, () => {
     console.log('Server is Runnig!');

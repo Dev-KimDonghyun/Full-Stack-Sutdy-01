5c4f616d5b61d39a2522eb957a94f9bdbfb48136
Dev-KimDonghyun <hyungus723@gmail.com>
2025-04-03 13:28:24 +0900

    Study: Login Processing and Encryption of Backend Express Setting

---
+++ Backend/routes/user.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const User = require('../models/User.js')
+
+// Express 라우터는 서버에서 HTTP 요청을 처리하고 각 요청에 맞는 응답을 보내는 역할을 함
+// 이게 RESTfulAPI의 기초가 되는 것 같음
+
+router.post('signUp', async ( res, req ) => {
+    try {
+        const { username, password } = req.body; // HTTP 요청의 본문에 담긴 데이터를 가져오는 역할을 함
+
+        const searchUser = await User.findOne({ username });
+        if (searchUser) {
+            return res.status(400).json({ message: '이미 존재하는 사용자입니다.'});
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // const user (하다가 중단함. Ch4-4. 관리자 계정 생성하기 6분 37초)
+    } catch (error) {
+
+    }
+})
